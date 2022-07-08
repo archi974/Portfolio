04a52b98155ad60b56fc9e44166740aa11e62a97
Vincent <jeremy.kerbidi@gmail.com>
2022-07-08 11:54:25 +0200

    [ADD] emailJS with secure data with .env

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 import Navbar from '../components/navbar/navbar';
+import emailjs from 'emailjs-com';
 import "./scss/contact.css";
 
 export default function Contact() {
+
+    function sendEmail(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        emailjs.sendForm(
+            process.env.REACT_APP_EMAILJS_SERVICE_ID,
+            process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+            e.currentTarget,
+            process.env.REACT_APP_EMAILJS_USER_ID
+        )
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.currentTarget.reset()
+    }
+
     return (
         <div>
             <Navbar />
-            <form name="contact" className="contact_form">
-                <h1>Me contacter :</h1>
-                
-                <input type="text" placeholder="Nom Prénom"/>
-
-                <input type="text" placeholder="Nom de l'entreprise"/>
-                
-                <input type="text" placeholder="Sujet" />
-
-                <input type="text" placeholder="Email"/>
-
-                <textarea rows={4} cols={50} maxLength={200} placeholder="Votre offre"></textarea>
+            <a href="../src/assets/Curriculum_Vitae_Vincent_K_BIDI.pdf" target="_blank" >Curriculum Vitae</a>
+            <form className="contact-form" onSubmit={sendEmail}>
+                <input type="text" name="from_name" placeholder="Nom" />
+                <input type="email" name="from_email" placeholder="Email" />
+                <textarea name="message" placeholder="Message..." />
+                <input type="submit" value="Send" />
             </form>
         </div>
-    )
+    );
 }
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="react-scripts" />
+export {};
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      REACT_APP_EMAILJS_SERVICE_ID: string;
+      REACT_APP_EMAILJS_TEMPLATE_ID: string;
+      REACT_APP_EMAILJS_USER_ID: string;
+    }
+  }
+}
