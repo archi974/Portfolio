2f0319e6543cd02a27f8745ad6dbfc49b6acece5
Vincent <jeremy.kerbidi@gmail.com>
2022-07-25 16:57:58 +0200

    [ADD/UPDATE] event scroll, css with query

--- src/component/Navbar/Navbar.tsx
+++ src/component/Navbar/Navbar.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
 export interface INavbarComponentProps { };
 
 const NavbarComponent: React.FunctionComponent<INavbarComponentProps> = props => {
+
+    const inputRef = useRef<HTMLHeadElement>(null);
+
+    useEffect(() => {
+        inputRef.current?.focus();
+        window.addEventListener('scroll', () => {
+            const scrollPos = window.scrollY;
+            console.log(scrollPos);
+
+            if (scrollPos > 10) {
+                inputRef.current?.classList.add('scrolled')
+            } else {
+                inputRef.current?.classList.remove('scrolled');
+            }
+        })
+    }, [])
+
     return (
-        <header className="main-header">
+        <header className="main-header" ref={inputRef}>
             <div className="logo">
                 <a href="/Portfolio/bio">LOGO</a>
             </div>
